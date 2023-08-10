67a1a53fbe80ce881c8f6e33254c8326e820b367
J-H-Cho <86359663+J-H-Cho@users.noreply.github.com>
2023-08-10 12:16:27 +0200

    Update Analyzer_to_BESAConnectivity.m
    source code cleanup

--- Additional_utilities/BESAConnectivityScripts/Analyzer_to_BESAConnectivity.m
+++ Additional_utilities/BESAConnectivityScripts/Analyzer_to_BESAConnectivity.m
@@ -1,13 +1,10 @@
-% Matlab_to_BESAConnectivity
+% Analyzer_to_BESAConnectivity
 %
-% Script to generate generate and export data from
-% MATLAB to BESA Connectivity.
+% Script to export Analyzer data from MATLAB to BESA Connectivity.
 %
-% It shows how to define required parameters, generates
-% example sine waves and calls the export function 
-% besa_save2Connectivity to store the simulated example 
-% data to disc. It will store the file containing the 
-% binary data matrix, as well as the header file and 
+% It shows how to define required parameters and calls the export function
+% besa_save2Connectivity to store Analyzer data to disc. It will store the
+% file containing the binary data matrix, as well as the header file and
 % channel description file in ASCII format.
 %
 % The script can be easily adopted to export your trial data 
@@ -16,7 +13,7 @@
 %
 % Copyright (C) 2023, BESA GmbH
 %
-% File name: Matlab_to_BESAConnectivity.m
+% File name: Analyzer_to_BESAConnectivity.m
 %
 %
 % Author: Robert Spangler and Mateusz Rusiniak
@@ -46,57 +43,50 @@ ChannelLabels = cell(NumChannels, 1);
 ChannelUnits = cell(NumChannels, 1);
 ChannelTypes = cell(NumChannels, 1);
 ChannelCoordinates = zeros(NumChannels, 3);
-for ChanIdx=1:NumChannels
+for ChanIdx = 1:NumChannels
     ChannelLabels{ChanIdx} = [EEG.chanlocs(ChanIdx).labels];
-    ChannelUnits{ChanIdx} = 'µV'; % 'nAm' 'µV'
+    ChannelUnits{ChanIdx} = 'ÂµV'; % 'nAm' 'ÂµV'
     if ~isempty(EEG.chanlocs(ChanIdx).sph_theta)
         ChannelTypes{ChanIdx} = 'EEG'; % 'DipSrc' 'POL' 'EEG' 'MEG'
         [ChannelCoordinates(ChanIdx,1),...
-     ChannelCoordinates(ChanIdx,2),...
-     ChannelCoordinates(ChanIdx,3)]=besa_transformCartesian2Spherical(...
-                        EEG.chanlocs(ChanIdx).Y,...
-                        EEG.chanlocs(ChanIdx).X,...
-                         EEG.chanlocs(ChanIdx).Z);
+         ChannelCoordinates(ChanIdx,2),...
+         ChannelCoordinates(ChanIdx,3)] = besa_transformCartesian2Spherical(...
+            EEG.chanlocs(ChanIdx).Y,...
+            EEG.chanlocs(ChanIdx).X,...
+            EEG.chanlocs(ChanIdx).Z);
     else
         ChannelTypes{ChanIdx} = 'POL';
     end
-    %EEG.chanlocs(ChanIdx).sph_phi];
-end    
+    %EEG.chanlocs(ChanIdx).sph_phi;
+end
 
 
-%% Sine waves
- TotalEpochLength = EpochLength(2) - EpochLength(1);
- NumberSamples = EEG.pnts;
- Latencies = linspace(EpochLength(1), EpochLength(2), NumberSamples) * 0.001;
- Data = cell(1, NumTrials);
- for TrialIdx=1:NumTrials
-     TrialData = zeros(NumChannels, NumberSamples);
-     for ChanIdx=1:NumChannels
-         TrialData(ChanIdx,:)=EEG.data(ChanIdx,:,TrialIdx);
-%         % Generate sine waves incl. noise
-%         AmplitudeSignal = 1;
-%         AmplitudeNoise = 0.2;
-%         Frequency = 5;
-%         TrialData(ChanIdx, :) = ...
-%             AmplitudeSignal*sin(2*pi*Frequency*Latencies) + ...
-%             AmplitudeNoise*randn(size(Latencies));
-     end    
-     Data{TrialIdx} = TrialData;
- end
+%% Data
+TotalEpochLength = EpochLength(2) - EpochLength(1);
+NumberSamples = EEG.pnts;
+Latencies = linspace(EpochLength(1), EpochLength(2), NumberSamples) * 0.001;
+Data = cell(1, NumTrials);
+for TrialIdx = 1:NumTrials
+    TrialData = zeros(NumChannels, NumberSamples);
+    for ChanIdx = 1:NumChannels
+        TrialData(ChanIdx,:)=EEG.data(ChanIdx,:,TrialIdx);
+    end
+    Data{TrialIdx} = TrialData;
+end
 
 
 %% Plot data of first trial
-%%{
-% MaxVal = max(max(abs(Data{1})));
-% for ChanIdx=1:NumChannels
-%     subplot(NumChannels, 1, ChanIdx);
-%     plot(Latencies, Data{1}(ChanIdx,:));
-%     hold on;
-%     plot([Latencies(1) Latencies(end)], [0 0], 'k');
-%     plot([0 0], [-MaxVal MaxVal], 'k');
-%     xlim([Latencies(1) Latencies(end)]);
-%     ylim([-MaxVal MaxVal]);
-% end
+%{
+MaxVal = max(max(abs(Data{1})));
+for ChanIdx = 1:NumChannels
+    subplot(NumChannels, 1, ChanIdx);
+    plot(Latencies, Data{1}(ChanIdx,:));
+    hold on;
+    plot([Latencies(1) Latencies(end)], [0 0], 'k');
+    plot([0 0], [-MaxVal MaxVal], 'k');
+    xlim([Latencies(1) Latencies(end)]);
+    ylim([-MaxVal MaxVal]);
+end
 %}
 
 
